5c76c4c3d64dd6594667f54fec00f8973dbb3afe
Fandi Kurnia <ria.komputer2006@gmail.com>
2018-06-29 13:46:34 +0700

    Update index.php
    add phpinfo

--- src/index.php
+++ src/index.php
@@ -31,5 +31,7 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
     </body>
-
+<?php 
+phpinfo();
+?>
 </html>
